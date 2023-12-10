1a81b45c6a338833dc07ef60b743ddb63e36b68d
David Cristofaro <david@dtcristo.com>
2023-12-10 15:54:13 +1100

    Comment day 9

--- 2023/src/bin/day_09_part_2.rs
+++ 2023/src/bin/day_09_part_2.rs
@@ -7,23 +7,34 @@ fn main() {
 }
 
 fn solution(input: &str) -> i32 {
+    // For each line of input.
     input
         .lines()
+        // Split whitespace and parse each value into `i32`.
         .map(|line| {
             line.split(' ')
                 .map(|value| value.parse().unwrap())
+                // Reverse values to extrapolate backwards.
+                // This line is the only change from part 1.
                 .rev()
                 .collect()
         })
+        // Extrapolate the values.
         .map(|values| extrapolate(values))
+        // Sum extrapolations.
         .sum()
 }
 
+// Recursively extrapolate a set of values.
 fn extrapolate(values: Vec<i32>) -> i32 {
+    // Base case when all values are 0, extrapolation is 0.
     if values.iter().all(|&value| value == 0) {
         0
     } else {
+        // Child values are calculated by the difference of each value pair.
         let children = values.iter().tuple_windows().map(|(a, b)| b - a).collect();
+        // Add the last value to the recursive extrapolation of the children
+        // to calucate this extrapolated value.
         values.last().unwrap() + extrapolate(children)
     }
 }
